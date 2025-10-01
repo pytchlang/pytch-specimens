2c0a44a1f8ed6d76200888e1f2bd519513ddae09
Ben North <ben.north@tcd.ie>
2025-10-01 11:27:43 +0100

    Swap file/link
    New behaviour:
    
        Write actual file to _by_content_hash_/<CONTENT_HASH>.zip
        Create symlink from, e.g., sbys/4.zip to that file

--- tools/make_bundle.py
+++ tools/make_bundle.py
@@ -41,15 +41,15 @@ def add_specimen(bundle_tar, root_dir, specimen_relative_path):
     hash = content_hash.project_content_hash(specimen_path)
     by_content_path = f"_by_content_hash_/{hash}.zip"
 
-    zip_tarinfo = tarfile.TarInfo(specimen_zip_path)
+    zip_tarinfo = tarfile.TarInfo(by_content_path)
     zip_tarinfo.size = len(specimen_zip_buf)
     zip_tarinfo.mode = 0o444
     zip_tarinfo.mtime = time.time()
     bundle_tar.addfile(zip_tarinfo, specimen_zip_io)
 
-    link_tarinfo = tarfile.TarInfo(by_content_path)
+    link_tarinfo = tarfile.TarInfo(specimen_zip_path)
     link_tarinfo.type = tarfile.SYMTYPE
-    link_tarinfo.linkname = f"../{specimen_zip_path}"
+    link_tarinfo.linkname = f"../{by_content_path}"
     link_tarinfo.mode = 0o444
     bundle_tar.addfile(link_tarinfo)
 
